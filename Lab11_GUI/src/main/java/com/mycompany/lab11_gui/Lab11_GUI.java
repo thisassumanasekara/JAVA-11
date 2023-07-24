cef4a21f9a8d66c9d6db701483a80bebdd2522d9
Thisas Sumanasekara <130208485+thisassumanasekara@users.noreply.github.com>
2023-07-24 21:27:42 +0530

    Add files via upload

---
+++ Lab11_GUI/src/main/java/com/mycompany/lab11_gui/Lab11_GUI.java
@@ -0,0 +1,10 @@
+package com.mycompany.lab11_gui;
+
+public class Lab11_GUI {
+
+    public static void main(String[] args) 
+    {
+        NewJFrame f=new NewJFrame ();
+        f.show();
+    }
+}
